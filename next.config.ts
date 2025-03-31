cc203d9ef79ea05fb71de741d5db7dc47b8945d6
Camilo Castillo <34112917+camilocas88@users.noreply.github.com>
2025-03-30 20:27:31 -0500

    Corregir configuración de workflow y Next.js para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
+  distDir: '.next',
   basePath: process.env.GITHUB_ACTIONS && '/camilo-castillo',
   images: {
     remotePatterns: [
